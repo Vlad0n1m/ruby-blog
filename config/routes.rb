7b4451665da9f8cbd64399ef0cfcde8a09090c05
Vlad0n1m <VladKarachkov321@gmail.com>
2023-06-28 10:45:11 +0600

    CRUD, Read completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'about', to: "pages#about"
   root 'pages#home'
+
+  get '/about', to: "pages#about"
+
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: "articles#detail"
 end
